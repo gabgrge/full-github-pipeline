3c337a21ff6208cae5863a5f779c483ee36dc8c8
gabgrge <gabriel.george@efrei.net>
2024-01-16 18:22:22 +0100

    Added files

--- pulled-app/app.py
+++ pulled-app/app.py
@@ -29,5 +29,5 @@ def update_item(index):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=5000)
+    app.run(debug=True, host='localhost', port=3000)
 
